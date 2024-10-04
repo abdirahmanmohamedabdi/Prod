7101f6913eaaf2f79a2cdaed39c71210ef367d1c
Abdirahman <abdirahmanabdi.ma@gmail.com>
2024-10-05 00:07:19 +0300

    created home,js page for the footer and navbar

---
+++ app/Home.js
@@ -0,0 +1,15 @@
+import Cta from "./components/Cta";
+import Features from "./components/Features";
+
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>
+        <Cta />
+        <Features />
+    </div>
+  )
+}
+
+export default Homepage
