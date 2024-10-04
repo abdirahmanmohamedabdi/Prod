43a5bf284709cc2c3c21ed533cb40ad1ee004e7b
Abdirahman <abdirahmanabdi.ma@gmail.com>
2024-10-05 00:07:47 +0300

    changed the layout to house the navbar and footer for all the pages

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
-
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 export const metadata = {
   title: "Pishipoa",
   description: "PishiPoa is your go-to platform for discovering, sharing, and exploring mouthwatering recipes",
@@ -10,8 +11,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        
+        <Navbar />
+      
         {children}
+        <Footer />
       </body>
     </html>
   );
