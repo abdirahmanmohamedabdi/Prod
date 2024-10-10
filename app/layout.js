beaf463b7a28e17653b6d1163baf296fce0555af
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-10-10 11:06:12 +0300

    feat: Add database connection and session provider to layout - Import and connect to MongoDB using connectDB - Wrap application with SessionProvider for session management - Include Navbar and Footer components in the layout

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,19 @@
 import "./globals.css";
 import { SessionProvider } from 'next-auth/react';
 import localFont from 'next/font/local';
+import { connectDB } from "./lib/mongo";
 import Navbar from "./components/Navbar";
-export default function Layout({ children, pageProps = {} }) {
+import Footer from "./components/Footer";
+export default async function  Layout({ children, pageProps = {} }) {
+  const conn = await connectDB();
+
   return (
     <html>
       <body>
         <SessionProvider session={pageProps.session}>
         <Navbar/>
         {children}
+        <Footer/>
         </SessionProvider>
       </body>
     </html>
