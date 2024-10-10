b2d4cb93154c84798f46673a83a164f0d4d64abc
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-10-10 11:05:40 +0300

    added credentials provider

--- app/auth.js
+++ app/auth.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GithhubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 export const {
     handlers: { GET, POST },
     auth,
