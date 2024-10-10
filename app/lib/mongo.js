2970bd3b6eee601ab02ff5b634a4759bd9fc2009
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-10-10 14:40:23 +0300

    Improve error handling in connectDB function - Ensure mongoose connection string is properly cast to a string - Improve error handling by throwing a new Error with the caught exception

--- app/lib/mongo.js
+++ app/lib/mongo.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connectDB() {
     try {
-        const conn = await mongoose.connect(String(process.env.MONGO_DB_CONNECTION_STRING));
+        let conn = await mongoose.connect(String(process.env.MONGO_DB_CONNECTION_STRING));
         return conn;
     } catch (e) {
         throw new Error(e);
