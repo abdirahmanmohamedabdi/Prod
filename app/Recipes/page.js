d8800cd142a2b21c7078d6b65cc92b67afdaa3b0
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-10-17 23:41:16 +0300

    cleaned up code for better styling

--- app/Recipes/page.js
+++ app/Recipes/page.js
@@ -1,5 +1,5 @@
 "use client";
-
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
 export default function RecipesPage() {
@@ -14,19 +14,19 @@ export default function RecipesPage() {
                     throw new Error('Failed to fetch recipes');
                 }
                 const data = await response.json();
-                setRecipes(data); // Set the recipes state with the fetched data
+                setRecipes(data); 
             } catch (error) {
                 setError(error.message);
             }
         };
 
-        fetchRecipes(); // Call the fetch function
+        fetchRecipes(); 
     }, []);
 
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-2xl font-bold font-font mb-4">Recipes</h1>
-            {error && <p className="text-red-500">Error: {error}</p>}
+            {error && <p className="text-red-500 font-font">Error: {error}</p>}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {recipes.length > 0 ? (
                     recipes.map((recipe) => (
@@ -38,10 +38,17 @@ export default function RecipesPage() {
                                     alt={recipe.title}
                                     className="w-full h-38 object-cover mb-4"
                                 />
-                                <p className='font-font'><strong>Ingredients:</strong> {recipe.ingredients.substring(0, 50)}...</p>
-                                <p className='font-font'><strong>Steps:</strong> {recipe.steps.substring(0, 50)}...</p>
+                               
                             </a>
-                            <p className="text-sm text-gray-500 font-font mt-2">Uploaded on: {new Date(recipe.createdAt).toLocaleDateString()}</p>
+                           
+                            <div className="flex justify-center items-center mt-4">
+                            <Link
+                                href={`/recipes/${encodeURIComponent(recipe._id)}`}
+                                className="px-4 py-2 bg-one font-font text-white rounded"
+                            >
+                                View Full Recipe
+                            </Link>
+                        </div>
                         </div>
                     ))
                 ) : (
