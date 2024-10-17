7f3a10640fba3d812f011d5fff582e6594dd37fe
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-10-17 20:38:37 +0300

    code to connect to mongodb and display the users favorite recipes

---
+++ app/api/favorites/list/route.js
@@ -0,0 +1,22 @@
+import { connectDB } from '@/app/lib/mongo';
+import { User } from '@/app/model/user-model';
+
+export async function GET(req) {
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get('userId'); 
+
+    try {
+        await connectDB(); 
+
+        
+        const user = await User.findById(userId).select('favorites').populate('favorites');
+        
+        if (!user) {
+            return new Response(JSON.stringify({ error: 'User not found' }), { status: 404 });
+        }
+
+        return new Response(JSON.stringify(user.favorites), { status: 200 });
+    } catch (error) {
+        return new Response(JSON.stringify({ error: 'Failed to fetch favorites' }), { status: 500 });
+    }
+}
